73d85ed57aae37166d3f46b970cf52225690bcf2
BranisGh <branisghoul02@hotmail.com>
2023-09-18 07:32:20 +0100

    add data ingestion modul

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -40,12 +40,11 @@ class DataIngestion:
 
             test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
 
-            logger.info("Inmgestion of the data iss completed")
+            logger.info("Ingestion of the data is completed")
 
             return(
                 self.ingestion_config.train_data_path,
                 self.ingestion_config.test_data_path
-
             )
         except Exception as e:
             raise CustomException(e,sys)
