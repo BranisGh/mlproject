af73689565aa46f5a10c06d9dea81d4b340e4ffc
BranisGh <branisghoul02@hotmail.com>
2023-09-19 14:57:27 +0100

    add artifacts and update all files

---
+++ test.py
@@ -0,0 +1,5 @@
+"""3. 
+# Ideas
+    - Add template.py to generate all project files and directories
+    - Add docker file
+    - Refer to the data science project repo to improve the ml model creation noteboock """
