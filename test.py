c4f4c52a3054e026d3bffc77bd69a5cabec3af2e
BranisGH <branisghoul02@hotmail.com>
2023-09-20 13:31:07 +0200

    update all files

--- test.py
+++
@@ -1,5 +0,0 @@
-"""3. 
-# Ideas
-    - Add template.py to generate all project files and directories
-    - Add docker file
-    - Refer to the data science project repo to improve the ml model creation noteboock """
